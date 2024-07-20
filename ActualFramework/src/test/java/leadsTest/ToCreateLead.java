2523fdf3fb1c5090145ae12b284d646b4503c63f
SumanShekhar2504 <poojasuman804404@gmail.com>
2024-07-20 09:46:42 +0530

    Added Testscript

---
+++ ActualFramework/src/test/java/leadsTest/ToCreateLead.java
@@ -0,0 +1,10 @@
+package leadsTest;
+
+public class ToCreateLead {
+
+	public static void main(String[] args) {
+
+		System.out.println("Lead Created");
+	}
+
+}
--- ActualFramework/src/test/java/organizationTest/ToCreateOrganization.java
+++ ActualFramework/src/test/java/organizationTest/ToCreateOrganization.java
@@ -5,6 +5,7 @@ public class ToCreateOrganization {
 	public static void main(String[] args) {
 
 		System.out.println("Organization");
+		System.out.println("Organization Created Successfully");
 	}
 
 }
